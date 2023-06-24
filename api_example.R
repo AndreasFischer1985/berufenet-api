1393d6e706db97d9ef0df9eee269edc8fb6c6d74
Dr. Andreas Fischer <45405681+AndreasFischer1985@users.noreply.github.com>
2023-06-24 14:31:11 +0200

    Update api_example.R

--- api_example.R
+++ api_example.R
@@ -5,14 +5,14 @@
 install.packages(c("devtools","jsonlite","httr","stringr"))
 devtools::install_github("AndreasFischer1985/qqBaseX")
 
-# Get API-Key
-#-------------
+# BERUFENET-API-Key:
+#-------------------
 x=httr::GET("https://web.arbeitsagentur.de/berufenet/ergebnisseite/berufe-a-z?buchstabe=a&page=0")
 xApiKey=stringr::str_match_all(httr::content(x,"text"),"client.*'")[[1]][,1]
 xApiKey=substr(xApiKey,12,nchar(xApiKey)-1)
 
-# Alle Berufe
-#-------------
+# BERUFENET-Berufsliste:
+#-----------------------
 url="https://rest.arbeitsagentur.de/infosysbub/bnet/pc/v1/berufe?suchwoerter=*"
 x=rawToChar(httr::content(httr::GET(url, httr::add_headers("X-API-Key"="d672172b-f3ef-4746-b659-227c39d95acf"))))
 pages=jsonlite::fromJSON(x)$page$totalPages
@@ -25,20 +25,9 @@ unlist(sapply(x,function(x)jsonlite::fromJSON(x)$'_embedded'$berufSucheList[,2])
 colnames(y)=c("id","kurzBezeichnungNeutral")
 write.csv2(y,"berufe.csv")
 
-# Ind. Berufsdaten
-#-----------------------
-url="https://rest.arbeitsagentur.de/infosysbub/bnet/pc/v1/berufe/27272"
+# BERUFENET-Berufsinformationen:
+#-------------------------------
+url="https://rest.arbeitsagentur.de/infosysbub/bnet/pc/v1/berufe/129987"
 x=httr::content(httr::GET(url, httr::add_headers("X-API-Key"="d672172b-f3ef-4746-b659-227c39d95acf")))
-
-x=lapply(y$id, function(id){
-	print(id);
-	httr::content(
-	httr::GET(paste0("https://rest.arbeitsagentur.de/infosysbub/bnet/pc/v1/berufe/",id), 
-	httr::add_headers("X-API-Key"="d672172b-f3ef-4746-b659-227c39d95acf")))})
-
-saveRDS(x,"berufenet-infos.rds")
-j=jsonlite::toJSON(x) #pretty=TRUE,auto_unbox=TRUE)
-
-writeLines(j, "berufenet-infos.json", useBytes=T)
-writeLines(jsonlite::toJSON(x[[1]]), "berufenet-infos_bsp1.json", useBytes=T)
+writeLines(jsonlite::toJSON(x), "berufenet-infos_bsp1.json", useBytes=T)
 
