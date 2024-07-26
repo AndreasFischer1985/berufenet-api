fc11b8629cf7e85feff929f8e58447339dba9f3c
Dr. Andreas Fischer <45405681+AndreasFischer1985@users.noreply.github.com>
2024-07-26 08:25:57 +0200

    Add files via upload

--- api_example.R
+++ api_example.R
@@ -14,10 +14,10 @@ xApiKey=substr(xApiKey,12,nchar(xApiKey)-1)
 # BERUFENET-Berufsliste:
 #-----------------------
 url="https://rest.arbeitsagentur.de/infosysbub/bnet/pc/v1/berufe?suchwoerter=*"
-x=rawToChar(httr::content(httr::GET(url, httr::add_headers("X-API-Key"="d672172b-f3ef-4746-b659-227c39d95acf"))))
+x=rawToChar(httr::content(httr::GET(url, httr::add_headers("X-API-Key"=xApiKey))))
 pages=jsonlite::fromJSON(x)$page$totalPages
 x=lapply(0:(pages-1),function(x) rawToChar(httr::content(httr::GET(paste0(url,"&page=",x), 
-	httr::add_headers("X-API-Key"="d672172b-f3ef-4746-b659-227c39d95acf")))))
+	httr::add_headers("X-API-Key"=xApiKey)))))
 
 y=data.frame(
 unlist(lapply(x,function(x)jsonlite::fromJSON(x)$'_embedded'$berufSucheList[,1])),
@@ -28,6 +28,6 @@ write.csv2(y,"berufe.csv")
 # BERUFENET-Berufsinformationen:
 #-------------------------------
 url="https://rest.arbeitsagentur.de/infosysbub/bnet/pc/v1/berufe/129987"
-x=httr::content(httr::GET(url, httr::add_headers("X-API-Key"="d672172b-f3ef-4746-b659-227c39d95acf")))
+x=httr::content(httr::GET(url, httr::add_headers("X-API-Key"=xApiKey)))
 writeLines(jsonlite::toJSON(x), "berufenet-infos_bsp1.json", useBytes=T)
 
